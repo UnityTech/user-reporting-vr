d3dc8c2d882f7c7fd4f4f56424490ad580f6689a
bretth <bretth@unity3d.com>
2018-11-28 13:43:28 -0800

    Added project.

---
+++ Assets/DeviceScript.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections.Generic;
+using Unity.Cloud.UserReporting.Plugin;
+using UnityEngine;
+
+public class DeviceScript : MonoBehaviour
+{
+    #region Constructors
+
+    public DeviceScript()
+    {
+        this.summaries = new List<string>();
+        this.summaries.Add("Audio Issue");
+        this.summaries.Add("Graphics Issue");
+        this.summaries.Add("Physics Issue");
+    }
+
+    #endregion
+
+    #region Fields
+
+    private bool isSubmitting;
+
+    public RenderTexture RenderTexture;
+
+    public TextMesh StatusText;
+
+    private List<string> summaries;
+
+    private int summaryIndex;
+
+    public TextMesh SummaryText;
+
+    #endregion
+
+    #region Methods
+
+    public void OnSubmit()
+    {
+        if (this.isSubmitting)
+        {
+            return;
+        }
+        this.isSubmitting = true;
+        UnityUserReporting.CurrentClient.TakeScreenshotFromSource(2048, 2048, this.RenderTexture, s => { });
+        UnityUserReporting.CurrentClient.TakeScreenshotFromSource(512, 512, this.RenderTexture, s => { });
+        UnityUserReporting.CurrentClient.CreateUserReport((br) =>
+        {
+            UnityUserReporting.CurrentClient.SendUserReport(br, (success, br2) =>
+            {
+                this.isSubmitting = false;
+            });
+        });
+    }
+
+    public void OnSummaryChange()
+    {
+        this.summaryIndex++;
+        if (this.summaryIndex >= this.summaries.Count)
+        {
+            this.summaryIndex = 0;
+        }
+    }
+
+    private void Update()
+    {
+        if (this.SummaryText != null)
+        {
+            this.SummaryText.text = this.summaries[this.summaryIndex];
+        }
+
+        if (this.StatusText != null)
+        {
+            this.StatusText.text = this.isSubmitting ? "Sending" : "Ready";
+        }
+    }
+
+    #endregion
+}
---
+++ Assets/Spawner.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using Unity.Cloud.UserReporting;
+using Unity.Cloud.UserReporting.Plugin;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class Spawner : MonoBehaviour
+{
+    #region Fields
+
+    private int cubeCount;
+
+    public Material material;
+
+    public Camera CameraSource;
+
+    private float duration;
+
+    private Queue<GameObject> spawnedCubes;
+
+    #endregion
+
+    #region Methods
+
+    private void SpawnCube()
+    {
+        GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        Vector2 positionInCirle = Random.insideUnitCircle;
+        positionInCirle *= 100;
+        cube.transform.position = new Vector3(positionInCirle.x, 100, positionInCirle.y);
+        Renderer cubeRenderer = cube.GetComponent<Renderer>();
+        cubeRenderer.material = this.material;
+        Rigidbody rigidBody = cube.AddComponent<Rigidbody>();
+        rigidBody.velocity = new Vector3((Random.value - 0.5F) * 10, 0, (Random.value - 0.5F) * 10);
+        rigidBody.angularVelocity = new Vector3((Random.value - 0.5F) * Mathf.PI * 10, (Random.value - 0.5F) * Mathf.PI * 10, (Random.value - 0.5F) * Mathf.PI * 10);
+        this.cubeCount++;
+        this.spawnedCubes.Enqueue(cube);
+        if (this.spawnedCubes.Count > 300)
+        {
+            GameObject spawnedCube = this.spawnedCubes.Dequeue();
+            Destroy(spawnedCube);
+        }
+    }
+
+    private void Start()
+    {
+        this.spawnedCubes = new Queue<GameObject>();
+    }
+
+    private void Update()
+    {
+        this.duration += Time.deltaTime;
+        if (this.duration > 0.1F)
+        {
+            this.duration = 0;
+            this.SpawnCube();
+        }
+    }
+
+    #endregion
+}
